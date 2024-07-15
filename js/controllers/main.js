a234af8bdfdb6d8ac50aeab5732a183ea8294bfb
guillermotoppph <guillermotoppph@gmail.com>
2024-07-15 10:51:20 -0300

    Cambie el nombre del html principal a index, agregue la carpeta js y service y controllers

---
+++ js/controllers/main.js
@@ -0,0 +1,8 @@
+const productosContainer = document.querySelector("data-product");
+
+function createCard (name, price, image, id){
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = 
+}
