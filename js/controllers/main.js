a7bdc3f698fd46273ed533c766cfeae1f41ef17e
guillermotoppph <guillermotoppph@gmail.com>
2024-07-15 12:33:48 -0300

    dejaron de renderizarse lo componentes

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -2,6 +2,8 @@ import { servicesProducts } from "../services/productservices.js";
 
 const productosContainer = document.querySelector("[data-product]");
 
+const form = document.querySelector("[data-form]");
+
 function createCard(name, price, image, id) {
     const card = document.createElement("div");
     card.classList.add("card");
@@ -46,5 +48,17 @@ const render = async () => {
         console.log(error);
     }
 };
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const name = document.querySelector("[data-name]").value;
+    const price = document.querySelector("[data-price]").value;
+    const image = document.querySelector("[data-image]").value;
+
+    console.log(name);
+    console.log(price);
+    console.log(image);
+});
 
 render();
